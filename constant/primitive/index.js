ce09b2570e8517dc386fd545f664ba2d8e4ae373
manokrajin <mahran.raja@gmail.com>
2022-09-16 02:13:52 +0700

    inititative : init design system - button component - primitive color - primitive font size

---
+++ constant/primitive/index.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const color = {
+    primary: '#F7D100',
+    secondary: '#4CB654',
+    background: '#FFFFFF',
+}
+
+
+
+const primitive = {
+    h1: "6.857rem",
+    h2: "4.286rem",
+    h3: "3.429rem",
+    h4: "2.429rem",
+    h5: "1.714rem",
+    h6: "1.429rem",
+    title: "1.143rem",
+    body: "1rem",    
+    caption: "0.857rem",
+    overline: "0.714rem",
+    tiny: "0.643rem",
+    extratiny: "0.6rem"
+};
+
+export {primitive, color};
