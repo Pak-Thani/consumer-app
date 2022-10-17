ccb1929f2f79ce64580decebc79514f2b4eb0da3
muhammadhafizm <76779078+muhammadhafizm@users.noreply.github.com>
2022-10-17 14:17:44 +0700

    FIX: Carousel next prev button logic (#25)
    * FIX: Carousel next prev button logic
    
    * FIX: minor bugs

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -4,46 +4,69 @@ import { useState, useEffect } from "react";
 import ArrowBack from "../../public/images/arrow-back.svg";
 
 export default function Carousel() {
-  const [index, setIndex] = useState(0);
-  const array = ["/3.jpg", "/6.jpg", "/2.jpg", "/5.jpg", "/1.jpg"];
+  const [carouselImageListIndex, setCarouselImageListIndex] = useState(0);
+  const carouselImageList = ["/3.jpg", "/6.jpg", "/2.jpg", "/5.jpg", "/1.jpg"];
 
-  function loop(count) {
-    if (count == array.length) {
-      return (count = 0);
-    }
-    if (count < 0) {
-      return (count = array.length - 1);
-    }
-    return count;
-  }
+  // todo @angle: Fix this logic
+  // function loop(count) {
+  //   if (count == array.length) {
+  //     return (count = 0);
+  //   }
+  //   if (count < 0) {
+  //     return (count = array.length - 1);
+  //   }
+  //   return count;
+  // }
 
-  useEffect(() => {
-    const interval = setInterval(
-      () => (setIndex) => (count) => (loop) => count + 1,
-      2000
-    );
-    return () => clearInterval(interval);
-  });
+  // todo @angle: Fix this logic
+  // useEffect(() => {
+  //   const interval = setInterval(
+  //     () => (setCarouselImageListIndex) => (count) => (loop) => count + 1,
+  //     2000
+  //   );
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  const handleOnClickPrevCarousel = () => {
+    if (carouselImageListIndex === 0) {
+      setCarouselImageListIndex(carouselImageList.length - 1);
+    } else {
+      setCarouselImageListIndex(carouselImageListIndex - 1);
+    }
+  };
+
+  const handleOnClickNextCarousel = () => {
+    if (carouselImageListIndex === carouselImageList.length - 1) {
+      setCarouselImageListIndex(0);
+    } else {
+      setCarouselImageListIndex(carouselImageListIndex + 1);
+    }
+  };
 
   return (
     <div className={styles.main}>
       <div
         className={`${styles.prev} ${styles.button}`}
-        onClick={() => setIndex((count) => count - 1)}
+        onClick={handleOnClickPrevCarousel}
       >
         <ArrowBack />
       </div>
 
       <div
         className={`${styles.next} ${styles.button}`}
-        onClick={() => setIndex((count) => count + 1)}
+        onClick={handleOnClickNextCarousel}
       >
         <ArrowBack />
       </div>
 
       <div className={styles.container}>
         <div className={styles.carouselImageWrapper}>
-          <Image src={array[index]} layout="fill" objectFit="cover" alt=" " />
+          <Image
+            src={`${carouselImageList[carouselImageListIndex]}`}
+            layout="fill"
+            objectFit="cover"
+            alt=" "
+          />
         </div>
       </div>
     </div>
