7204e3e9e18ca837fd1c40e1324c63f5855a6b71
muhammadhafizm <76779078+muhammadhafizm@users.noreply.github.com>
2022-10-18 10:16:50 +0700

    [feature] Integrate Product Detail with BE API (#27)
    - use client side rendering
    - get productSlug from url query
    - create getProductByProductSlug method to fetching data
    - create temp loading component **(need to change this)**

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
-import constant from "./constant"
+import constant from "./constant";
 import { fetchGet } from "./utils";
 
-const { CONSUMER_API_BASE_URL } = constant
+const { CONSUMER_API_BASE_URL } = constant;
 
 export async function getAllCustomSection() {
-    return fetchGet(`${CONSUMER_API_BASE_URL}/api/custom-sections/`);
-  }
+  return fetchGet(`${CONSUMER_API_BASE_URL}/api/custom-sections/`);
+}
+
+export async function getProductByProductSlug(slug) {
+  return fetchGet(`${CONSUMER_API_BASE_URL}/api/product/${slug}`);
+}
--- pages/product/index.js
+++ pages/product/index.js
@@ -2,18 +2,38 @@ import styles from "./index.module.css";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import fotoproduct from "../../public/bayam.png";
-import checkmark from "../../public/Checkmark.png";
 import ArrowBackBlack from "../../public/images/arrow-back-black.svg";
-import axios from "axios";
 import { useRouter } from "next/router";
+import { getProductByProductSlug } from "../../api";
+import { numberPriceToStringPrice } from "../../utils/productUtils";
 
 export default function ProductDetail() {
   const [isOpen, setIsOpen] = useState(false);
+  const [productData, setProductData] = useState(undefined);
   const openMenu = () => setIsOpen(!isOpen);
 
   const route = useRouter();
-  
-  console.log(route.query)
+  const {
+    query: { productSlug },
+  } = route;
+
+  // you can start to explore data fetchin in next
+  // instead we use client side data fetching
+  // you can try another method data fetching that suitable for your case
+  // i use this method to make me easier fetch data from client
+  // if we can fetch data from server, we can reduce loading time and user experience
+  useEffect(async () => {
+    if (productSlug) {
+      const data = await getProductByProductSlug(productSlug);
+      setProductData(data);
+    }
+  }, [productSlug]);
+
+  // please change this loading screen immediately
+  // you can use temp data
+  // or maybe use screen loading or skeleton component to make it more pretty
+  // todo @angle: change this boring loading component 
+  if (!productData) return <h1>loading</h1>;
 
   return (
     <div className={styles.container}>
@@ -21,21 +41,20 @@ export default function ProductDetail() {
         <div className={styles.backButton} onClick={() => route.push("/")}>
           <ArrowBackBlack />
         </div>
-        <Image layout="fill" objectFit="cover" src={fotoproduct} />
+        <Image layout="fill" objectFit="cover" src={productData.data.image} />
       </div>
       <div className={styles.productBody}>
-        <h2 className={styles.displayName}>Bayam Brazil</h2>
+        <h2 className={styles.displayName}>{productData.data.name}</h2>
         <div className={styles.priceWrapper}>
-          <h2 className={styles.price}>Rp. 45.000</h2>
-          <h4 className={styles.qty}>/ 1 kg</h4>
+          <h2 className={styles.price}>
+            {numberPriceToStringPrice(productData.data.pricePerQty)}
+          </h2>
+          <h4 className={styles.qty}>/ {productData.data.qty}</h4>
         </div>
         <p className={styles.productDetailTitle}>Detail Produk</p>
-        <p className={styles.productDetail}>
-          Daging sapi yang berasal dari Amazon dengan tekstur kenyal yang sangat
-          disukai oleh penggemar steak!
-        </p>
+        <p className={styles.productDetail}>{productData.data.description}</p>
         <p className={styles.variant}>Pilih Varian</p>
-        <div className={styles.variantSelection}>1 kg</div>
+        <div className={styles.variantSelection}>{productData.data.qty}</div>
       </div>
       <div className={styles.buyButtonWrapper}>
         <div className={styles.buyButton} onClick={openMenu}>
