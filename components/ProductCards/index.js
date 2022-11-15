14a7b1ded89d25278b49e33a4dfc1f2de308d4b8
Aryanda Rizky P <aryandarizky1234@gmail.com>
2022-11-16 03:41:00 +0700

    [FEATURE] membedakan card dengan stockavailable

--- components/ProductCards/index.js
+++ components/ProductCards/index.js
@@ -5,35 +5,75 @@ import styles from "./index.module.css";
 function ProductCards({ product }) {
   const router = useRouter();
 
-
   return (
-    <div
-      className={styles.card}
-      key={product.name}
-      onClick={() =>
-        router.push({
-          pathname: "/product",
-          query: { productSlug: product.slug },
-        })
-      }
-    >
-      <div className={styles.cardImgTop}>
-        <Image
-          src={product.image}
-          layout="fill"
-          objectFit="cover"
-          className={styles.skeleton}
-        />
-      </div>
-      <div className={styles.cardBottomSection}>
-        <h5 className={styles.cardTitle}>{product.name}</h5>
-        <p className={styles.cardText}>
-          Rp {product.pricePerQty}
-          <span> / {product.qty}</span>
-        </p>
-        <div className={styles.btn}>Beli</div>
-      </div>
-    </div>
+    <>
+      {product.isStockAvailable ? (
+        <div
+          className={styles.card}
+          key={product.name}
+          onClick={() =>
+            router.push({
+              pathname: "/product",
+              query: { productSlug: product.slug },
+            })
+          }
+        >
+          <div className={styles.cardImgTop}>
+            {product.stockAvailable < 10 ? (
+              <div className={styles.textContainer}>
+                <p className={styles.sisaText}>
+                  Sisa {product.stockAvailable}!
+                </p>
+              </div>
+            ) : (
+              <></>
+            )}
+            <Image
+              src={product.image}
+              layout="fill"
+              objectFit="cover"
+              className={styles.skeleton}
+            />
+          </div>
+          <div className={styles.cardBottomSection}>
+            <h5 className={styles.cardTitle}>{product.name}</h5>
+            <p className={styles.cardText}>
+              Rp {product.pricePerQty}
+              <span> / {product.qty}</span>
+            </p>
+            <div className={styles.btn}>Beli</div>
+          </div>
+        </div>
+      ) : (
+        <div
+          className={styles.cardEmpty}
+          key={product.name}
+          onClick={() =>
+            router.push({
+              pathname: "/product",
+              query: { productSlug: product.slug },
+            })
+          }
+        >
+          <div className={styles.cardImgTop}>
+            <Image
+              src={product.image}
+              layout="fill"
+              objectFit="cover"
+              className={styles.skeleton}
+            />
+          </div>
+          <div className={styles.cardBottomSection}>
+            <h5 className={styles.cardTitle}>{product.name}</h5>
+            <p className={styles.cardText}>
+              Rp {product.pricePerQty}
+              <span> / {product.qty}</span>
+            </p>
+            <div className={styles.btnEmpty}>Beli</div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
