a9878dc1626a1a5a46723daa2bbda207cef3885b
Angelia Regina <angeliardk2002@gmail.com>
2022-11-29 20:06:48 +0700

    caraousel-animation

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -2,13 +2,20 @@ import styles from "./index.module.css";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import ArrowBack from "../../public/images/arrow-back.svg";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, EffectFade } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { useRef } from "react";
 
 export default function Carousel() {
   const [carouselImageListIndex, setCarouselImageListIndex] = useState(0);
   const carouselImageList = ["/3.jpg", "/6.jpg", "/2.jpg", "/5.jpg", "/1.jpg"];
-  const delay = 2500;
-  const [isloading] = useState(true);
-  
+  const delay = 5000;
+  const swiperNavPrevRef = useRef(null)
+  const swiperNavNextRef = useRef(null)
+
   useEffect(() => {
     setTimeout(
       () =>
@@ -21,47 +28,64 @@ export default function Carousel() {
     return () => {};
   }, [carouselImageListIndex]);
 
-  const handleOnClickPrevCarousel = () => {
-    if (carouselImageListIndex === 0) {
-      setCarouselImageListIndex(carouselImageList.length - 1);
-    } else {
-      setCarouselImageListIndex(carouselImageListIndex - 1);
-    }
-  };
+  // const handleOnClickPrevCarousel = () => {
+  //   if (carouselImageListIndex === 0) {
+  //     setCarouselImageListIndex(carouselImageList.length - 1);
+  //   } else {
+  //     setCarouselImageListIndex(carouselImageListIndex - 1);
+  //   }
+  // };
 
-  const handleOnClickNextCarousel = () => {
-    if (carouselImageListIndex === carouselImageList.length - 1) {
-      setCarouselImageListIndex(0);
-    } else {
-      setCarouselImageListIndex(carouselImageListIndex + 1);
-    }
-  };
+  // const handleOnClickNextCarousel = () => {
+  //   if (carouselImageListIndex === carouselImageList.length - 1) {
+  //     setCarouselImageListIndex(0);
+  //   } else {
+  //     setCarouselImageListIndex(carouselImageListIndex + 1);
+  //   }
+  // };
 
   return (
     <div className={styles.main}>
-      <div
-        className={`${styles.prev} ${styles.button}`}
-        onClick={handleOnClickPrevCarousel}
-      >
-        <ArrowBack />
-      </div>
-
-      <div
-        className={`${styles.next} ${styles.button}`}
-        onClick={handleOnClickNextCarousel}
-      >
-        <ArrowBack />
-      </div>
-
       <div className={styles.container}>
         <div className={styles.carouselImageWrapper}>
-          <Image
-            src={`${carouselImageList[carouselImageListIndex]}`}
-            layout="fill"
-            objectFit="cover"
-            alt=" "
-            className={styles.skeleton}
-          />
+          <Swiper
+            modules={[Navigation, EffectFade]}
+            navigation
+            
+            // navigation={{
+            //   prevEl: swiperNavPrevRef.current,
+            //   nexrEl: swiperNavNextRef.current,
+            // }}
+            effect
+            slidesPerView={1}
+            speed={800}
+            loop
+            className={styles.mySwiper}
+          > 
+
+          {/* <div
+            className={styles.swiperNavPrev} ref={swiperNavPrevRef}>
+          </div>
+          <div
+            className={styles.swiperNavNext} ref={swiperNavNextRef}>
+          </div> */}
+
+          <SwiperSlide >
+            <div className={styles.carouselImageWrapper}>
+              <Image
+                src={`${carouselImageList[carouselImageListIndex]}`}
+                layout="fill"
+                objectFit="cover"
+                effect={'fade'}
+                slidesPerView={1}
+                speed={800}
+                loop
+                alt=" "
+                className={styles.skeleton}
+              />
+            </div>
+          </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </div>
