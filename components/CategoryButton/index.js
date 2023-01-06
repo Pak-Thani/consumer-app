01e71b310299dc452ee1cd1834d2bd17cf806999
Aryanda04 <aryandarizky1234@gmail.com>
2023-01-06 18:43:28 +0700

    change phone number

--- components/CategoryButton/index.js
+++ components/CategoryButton/index.js
@@ -10,31 +10,37 @@ import saladLogo from "../../public/Salad.png";
 const CategoryButton = ({ data }) => {
   const router = useRouter();
 
-
   return (
     <>
-        <div
-      key={data.name}
-      onClick={() =>
-        router.push({
-          pathname: `/category`,
-          query: { categorySlug : data.slug },
-        })
-      }
-    >
-
-      <div className={styles.catergoryWrapper}>
-        <div className={styles.button}>
-          <div className={styles.logoWrapper}>
-            <div style={{width: '87px', height: '87px', position: 'relative'}}>
-        <Image src={data.image} className={styles.skeleton} layout="fill"
-    sizes='100vm' />
-      </div>
+      <div
+        key={data.name}
+        onClick={() =>
+          router.push({
+            pathname: `/category`,
+            query: { categorySlug: data.slug },
+          })
+        }
+      >
+        <div className={styles.catergoryWrapper}>
+          <div className={styles.button}>
+            <div className={styles.logoWrapper}>
+              <div
+                style={{ width: "87px", height: "87px", position: "relative" }}
+              >
+                <Image
+                  src={data.image}
+                  className={styles.img}
+                  // layout="fill"
+                  // sizes="100vm"
+                  width={87}
+                  height={87}
+                />
+              </div>
+            </div>
           </div>
+          <p className={styles.categoryName}>{data.name}</p>
         </div>
-        <p className={styles.categoryName}>{data.name}</p>
       </div>
-    </div>
     </>
   );
 };
