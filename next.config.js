32f79d6e0cb12d84b5db48e273920f4a0f79eb52
Aryanda04 <aryandarizky1234@gmail.com>
2023-01-20 01:03:13 +0700

    add new config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   images: {
-    domains: ["pakthani.s3.amazonaws.com"],
+    domains: [
+      "pakthani.s3.amazonaws.com",
+      "pakthani.s3.ap-southeast-1.amazonaws.com",
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ];
   },
   webpack(config) {
     config.module.rules.push({
